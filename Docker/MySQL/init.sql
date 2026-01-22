2ff6c1d3b428dd9adc18f2f7addec0163db86a73
tahira1321 <71886042+tahira1321@users.noreply.github.com>
2026-01-22 17:23:07 +0900

    Feature/tahira init.sqlの内容の編集と動作の確認 (#9)
    * tahira テスト入力
    
    * feat:tahira init.sqlの作成と動作の確認
    
    * phpmyadminのファイルが削除されてしまったので追加
    
    ---------
    
    Co-authored-by: kazuki

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -22,7 +22,8 @@ CREATE TABLE
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
-        created_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+	introduce TEXT NULL,
+	icon_file_name VARCHAR(255) NULL,
         updated_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
         PRIMARY KEY (id),
         UNIQUE KEY uq_users_email (email)
@@ -32,7 +33,7 @@ CREATE TABLE
     posts (
         id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
         user_id BIGINT UNSIGNED NOT NULL,
-        content TEXT NOT NULL,
+        body TEXT NOT NULL,
         created_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
         updated_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
         deleted_at DATETIME (6) DEFAULT NULL,
@@ -46,7 +47,7 @@ CREATE TABLE
         id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
         user_id BIGINT UNSIGNED NOT NULL,
         post_id BIGINT UNSIGNED NOT NULL,
-        content TEXT NOT NULL,
+        body TEXT NOT NULL,
         created_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
         updated_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
         PRIMARY KEY (id),
@@ -57,18 +58,63 @@ CREATE TABLE
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 
+CREATE TABLE
+    likes (
+        id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        user_id BIGINT UNSIGNED NOT NULL,
+        post_id BIGINT UNSIGNED NULL,
+	comment_id BIGINT UNSIGNED NULL,
+        PRIMARY KEY (id),
+        KEY idx_likes_user_id (user_id),
+        KEY idx_likes_post_id (post_id),
+        KEY idx_likes_comment_id (comment_id),
+        CONSTRAINT fk_likes_user FOREIGN KEY (user_id) REFERENCES users (id),
+        CONSTRAINT fk_likes_post FOREIGN KEY (post_id) REFERENCES posts (id),
+        CONSTRAINT fk_likes_comment FOREIGN KEY (comment_id) REFERENCES comments (id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE
+    medias (
+        id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        user_id BIGINT UNSIGNED NOT NULL,
+        post_id BIGINT UNSIGNED NOT NULL,
+	file_name VARCHAR(255) NULL,
+        created_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        updated_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+        PRIMARY KEY (id),
+        KEY idx_medias_user_id (user_id),
+        KEY idx_medias_post_id (post_id),
+        CONSTRAINT fk_medias_user FOREIGN KEY (user_id) REFERENCES users (id),
+        CONSTRAINT fk_medias_post FOREIGN KEY (post_id) REFERENCES posts (id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE
+    post_medias (
+        id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        user_id BIGINT UNSIGNED NOT NULL,
+        post_id BIGINT UNSIGNED NOT NULL,
+	media_id BIGINT UNSIGNED NOT NULL,
+        PRIMARY KEY (id),
+        KEY idx_post_medias_conn_user_id (user_id),
+        KEY idx_post_medias_conn_post_id (post_id),
+        KEY idx_post_medias_conn_media_id (media_id),
+        CONSTRAINT fk_post_medias_user FOREIGN KEY (user_id) REFERENCES users (id),
+        CONSTRAINT fk_post_medias_post FOREIGN KEY (post_id) REFERENCES posts (id),
+        CONSTRAINT fk_post_medias_media FOREIGN KEY (media_id) REFERENCES medias (id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
 INSERT INTO users (name, email, password)
 VALUES 
   ('山田太郎', 'taro@example.com', '937e8d5fbb48bd4949536cd65b8d35c426b80d2f830c5c308e2cdec422ae2244'),
   ('鈴木二郎', 'jiro@example.com', '937e8d5fbb48bd4949536cd65b8d35c426b80d2f830c5c308e2cdec422ae2244');
 
-INSERT INTO posts (user_id, content)
+INSERT INTO posts (user_id, body)
 VALUES
   (1, 'こんにちは！初めての投稿です。'),
   (1, '今日はとても良い天気ですね。'),
   (1, '今日も勉強頑張ります！');
 
 
-INSERT INTO comments (user_id, post_id, content)
+INSERT INTO comments (user_id, post_id, body)
 VALUES
     (2, 1, '応援しています！頑張ってください。');
