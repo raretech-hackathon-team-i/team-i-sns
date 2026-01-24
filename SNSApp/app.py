752f7e4cdcf46f2e46e9dacca8d6b4376a7e96bd
asera1231 <tetsuchan78@gmail.com>
2026-01-24 12:46:39 +0900

    refactor: rename login to top and rewrite app.py

--- SNSApp/app.py
+++ SNSApp/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf.csrf import CSRFProtect
 import os
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY", "dev")
+csrf = CSRFProtect(app)
 
 posts = []
 
