38351c4ec271851b7bb82ba7ef64f6ac1b79ea19
tahira1321 <takuya.hirano.321+git@gmail.com>
2026-01-25 18:20:07 +0900

    feat/tahira models.pyに投稿関連のクラスを追加

--- SNSApp/models.py
+++ SNSApp/models.py
@@ -62,11 +62,87 @@ class User:
         finally:
             pool.release(conn)
 
-
 # 投稿クラス
 class Post:
-    pass
+    @classmethod
+    def get_all(cls):
+        conn = db_pool.get_conn()
+        try:
+            with conn.corsur() as cur:
+                sql = "SELECT * FROM posts WHERE deleted_at IS NULL ORDER BY created_at DESC;"
+                cur.execute(sql)
+                posts = cur.fetchall()
+            return posts
+        except pymysql.Error as e:
+            print(f'エラーが発生しています:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    # 投稿作成処理
+    @classmethod
+    def create(cls, user_id, body):
+        conn = db_pool.get_conn()
+        try:
+            with conn.corsur() as cur:
+                sql = "INSERT INTO posts (user_id, body) VALUES (%s, %s);"
+                cur.execute(sql, (user_id, body))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生しています:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    # 削除処理
+    @classmethod
+    def delete(cls, post_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.corsur() as cur:
+                sql = "UPDATE posts SET deleted_at = NOW() WHERE id = %s;"
+                cur.execute(sql, (post_id))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生しています:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def find_by_id(cls, post_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.corsur() as cur:
+                sql = "SELECT * FROM posts WHERE id=%s AND deleted_at IS NULL;"
+                cur.execute(sql, (post_id))
+                posts = cur.fetchone()
+            return posts
+        except pymysql.Error as e:
+            print(f'エラーが発生しています:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
 
 # コメントクラス
 class Comment:
     pass
+
+# いいねクラス
+""" 作成中
+class Like:
+    @classmethod
+    def get_count_by_post_id(cls, id):
+    pool = get_db_pool()
+    conn = pool.get_conn()
+    try:
+        with conn.cursor() as cur:
+            sql = "SELECT COUNT(*) FROM likes;"
+            cur.execute(sql, )
+            conn.commit
+    except pymysql.Error as e:
+        print(f'エラーが発生しています:{e}')
+        abort(500)
+    finally:
+"""
+        db_pool.release(conn)
